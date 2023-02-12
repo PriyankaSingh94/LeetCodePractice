1d95d713c07fc827c2d74829b1eec4a696fa9c6b
PriyankaSingh94 <raopriyankasingh94@gmail.com>
2023-02-12 19:00:10 +0530

    Valid Parenthesis

--- src/com/learn/leetcode/easy1/LongestComPrefix.java
+++ src/com/learn/leetcode/easy1/LongestComPrefix.java
@@ -4,9 +4,13 @@ public class LongestComPrefix {
 
 	public static void main(String[] args) {
 		
-		String[] strs = {"flight", "flow"};
+		String[] strs = {"flight", "flow", "flee", "flu"};
+		
+		longestCommonPrefix2(strs, 0 , strs.length - 1);
+		
 		longestCommonPrefix1(strs);
 
+		
 	}
 	
 	public static String longestCommonPrefix(String[] strs) {
@@ -42,6 +46,27 @@ public class LongestComPrefix {
 	            if (prefix.isEmpty()) return "";
 	        }        
 	    return prefix;
+	}
+	
+	private static String longestCommonPrefix2(String[] strs, int l, int r) {
+	    if (l == r) {
+	        return strs[l];
+	    }
+	    else {
+	        int mid = (l + r)/2;
+	        String lcpLeft =   longestCommonPrefix2(strs, l , mid);
+	        String lcpRight =  longestCommonPrefix2(strs, mid + 1,r);
+	        return commonPrefix(lcpLeft, lcpRight);
+	   }
+	}
+
+	static String commonPrefix(String left,String right) {
+	    int min = Math.min(left.length(), right.length());       
+	    for (int i = 0; i < min; i++) {
+	        if ( left.charAt(i) != right.charAt(i) )
+	            return left.substring(0, i);
+	    }
+	    return left.substring(0, min);
 	}
 
 }
